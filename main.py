6ff065707733ba542e50d28b93ba7b118426d2a6
Agustin Exequiel Gimenez <exequielgimenez026@gmail.com>
2025-06-06 14:49:05 -0300

    Implementacion de la declaracion 'Match' y solcuion de errores

--- main.py
+++ main.py
@@ -158,3 +158,9 @@ menu_principal()
 #Implementar condicion para que solo acepte los roles (Administrador o Estandar)
 #Implemnetar condicion para que no se pueda iniciar sesion sin completar los campos (campos vacios)
 #Implementar condicion para que dos usuarios no posean el mismo id_usuario
+
+
+
+
+
+
